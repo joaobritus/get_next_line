7bd1d0a2210f619d66f187ebe32ec5ebb6583894
“joaobritus” <joaoaragaobrito@gmail.com>
2022-12-19 15:25:28 +0000

    oladenovo

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,27 @@
 /*   By: jaragao- <jaragao-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/07 14:00:44 by jaragao-          #+#    #+#             */
-/*   Updated: 2022/12/13 17:08:17 by jaragao-         ###   ########.fr       */
+/*   Updated: 2022/12/19 15:24:59 by jaragao-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*ft_restart(char *buffer)
+{
+	char	*line;
+
+	line = malloc(ft_strlen(buffer) + 1);
+	if (ft_strchar(buffer, '\n'))
+	{
+		line = ft_strjoin(line, buffer);
+		ft_clean(line);
+		ft_index(buffer);
+		return (line);
+	}
+	line = ft_strjoin(line, buffer);
+	return (line);
+}
 
 char	*get_next_line(int fd)
 {
@@ -19,13 +35,11 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	line = NULL;
-	if (ft_strchar(buffer, '\n'))
+	if (buffer != '\0')
 	{
-		line = ft_strjoin(line, buffer);
-		ft_clean(line);
-		ft_index(buffer);
-		return (line);
+		line = ft_restart(buffer);
+		if (ft_strchar(line, '\n'))
+			return (line);
 	}
 	while (read(fd, buffer, BUFFER_SIZE) > 0)
 	{
@@ -40,7 +54,7 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-int	main(void)
+/*int	main(void)
 {
 	char	*line;
 	int		fd;
@@ -50,4 +64,4 @@ int	main(void)
 	line = get_next_line(fd);
 	printf("%s", line);
 	free(line);
-}
+}*/
